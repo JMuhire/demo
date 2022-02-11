961039c4196557cf528d86c6ab277ef4cd46e1fe
Muhire Jean <JMuhire>
2022-02-11 10:17:14 +0000

    Initial commit

---
+++ src/main/java/com/sparkers/Application.java
@@ -0,0 +1,10 @@
+package com.sparkers;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
